34fe908e69521ffea09eaa51145aa1d4643ee4c5
SadLunch <olaadeus12345z@gmail.com>
2024-10-13 16:05:34 +0100

    maybe now it will work

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -5,14 +5,11 @@ const FeedbackForm = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-  
-    const myForm = event.target;
-    const formData = new FormData(myForm);
     
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
+      body: new URLSearchParams(feedback),
     })
       .then(() => alert(`Form successfully submitted: ${feedback}`))
       .catch((error) => alert(error));
