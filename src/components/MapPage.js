717baf98d177cdc2a438f6cb0bef7cf03c9ef3dd
SadLunch <olaadeus12345z@gmail.com>
2024-10-17 14:27:26 +0100

    hardcoding experience in mapPage

--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -9,6 +9,10 @@ import { useNavigate } from 'react-router-dom';
 
 const socket = io.connect('https://reactgeolocation-backend.onrender.com'); // Replace with your backend URL
 
+const hardcodedExperiences = [
+  { id: 1, location: { lat: 38.710, lng: -9.140 }, name: 'Test Experience', nUsersIn: 5 }
+];
+
 // Set up the default icon for markers
 const DefaultIcon = L.icon({
   iconUrl: icon,
@@ -47,7 +51,7 @@ const MapPage = () => {
   useEffect(() => {
     socket.on('exp-location', (experiences) => {
       console.log("Received experiences:", experiences);
-      setExperienceLocations([...experiences]);
+      setExperienceLocations(hardcodedExperiences);
     });
   
     return () => {
