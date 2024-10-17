0cc1903167ecd8e599a5a1cb04aac853a8ec676f
SadLunch <olaadeus12345z@gmail.com>
2024-10-17 22:25:55 +0100

    taking away feedback form

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import FeedbackForm from '../components/FeedbackForm';
+//import FeedbackForm from '../components/FeedbackForm';
 
 const HomePage = () => (
   <div>
     <h1>Welcome to Geolocation App</h1>
     <Link to="/map">Go to Map</Link>
-    <FeedbackForm />
+    {/* <FeedbackForm /> */}
   </div>
 );
 
