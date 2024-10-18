dcc3ae399fc1d5bb45b0064595b1f854f36c6053
SadLunch <olaadeus12345z@gmail.com>
2024-10-18 14:20:34 +0100

    added function to remove local storage item when clicked

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,6 +6,9 @@ const HomePage = () => (
   <div>
     <h1>Welcome to Geolocation App</h1>
     <Link to="/map">Go to Map</Link>
+    <span onClick={() => {
+      localStorage.removeItem('uuid');
+    }} >Remove User ID from localstorage</span>
     {/* <FeedbackForm /> */}
   </div>
 );
